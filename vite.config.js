0acf82a42efb7288411a78afaeb223685c31e610
k-praneeth567 <praneethkothamaddi567@gmail.com>
2025-12-11 01:36:30 +0530

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -6,16 +6,11 @@ import react from '@vitejs/plugin-react'
 // - If it's a user/organization page (username.github.io), set base to '/'
 // - You can also use environment variable: VITE_BASE_PATH
 //   Example: VITE_BASE_PATH=/my-repo/ npm run build
-const getBasePath = () => {
-  // Check for environment variable first (Node.js process.env)
-  if (process.env.VITE_BASE_PATH) {
-    return process.env.VITE_BASE_PATH
-  }
+
   // Default to repository name (update this to match your GitHub repo name)
-  return ' /janaki-birthday-quest-transitions-goldcard/'
-}
+
 
 export default defineConfig({
   plugins: [react()],
-  base: getBasePath(),
+  base: 'Birthday_proj-1',
 })
